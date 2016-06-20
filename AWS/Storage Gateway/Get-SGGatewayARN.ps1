9a11cc4d69d200d40e7f5bc895cc960717a9cf38
Ross Hevener <rosshev@gmail.com>
2016-06-20 15:37:33 -0400

    Added -IncludeRegion parameter

--- AWS/Storage Gateway/Get-SGGatewayARN.ps1
+++ AWS/Storage Gateway/Get-SGGatewayARN.ps1
@@ -4,15 +4,31 @@
 
 param ([Parameter(Mandatory = $true)]
 	[string]$Name,
-	[string]$Region)
+	[string]$Region,
+	[switch]$IncludeRegion)
 
 # AWS Storage Gateway regions. Regions can be reordered/removed in the interest of saving clock cycles.
 $awsRegionList = 'us-east-1', 'us-west-1', 'us-west-2', 'eu-west-1', 'eu-central-1', 'ap-northeast-1', 'ap-northeast-2',
 'ap-southeast-1', 'ap-southeast-2', 'sa-east-1'
 
+# Create object if -IncludRegion is specified
+if ($IncludeRegion) { $gatewayInfo = new-object psobject }
+
 if ($region) # region specified, only the region in $Region will be searched
 {
 	$arn = (Get-SGGateway -Region $region | Where-Object GatewayName -eq $Name).GatewayArn
+	
+	if ($IncludeRegion) # -IncludeRegion specified
+	{
+		$gatewayInfo | Add-Member -MemberType NoteProperty -Name 'ARN' -Value $arn
+		$gatewayInfo | Add-Member -MemberType NoteProperty -Name 'Region' -Value $Region
+		
+		return $gatewayInfo
+	}
+	else # -IncludeRegion not specified
+	{
+		return $arn
+	}
 }
 else # region not specified, will loop through regions in $awsRegionList until a match is found
 {
@@ -21,8 +37,22 @@ else # region not specified, will loop through regions in $awsRegionList until a
 		$arn = (Get-SGGateway -Region $awsRegion | Where-Object GatewayName -eq $Name).GatewayArn
 		
 		# Exit loop when $arn is populated
-		if ($arn) { break }
+		if ($arn)
+		{
+			if ($IncludeRegion) # -IncludeRegion specified
+			{
+				$gatewayInfo | Add-Member -MemberType NoteProperty -Name 'ARN' -Value $arn
+				$gatewayInfo | Add-Member -MemberType NoteProperty -Name 'Region' -Value $AWSregion
+				
+				return $gatewayInfo
+			}
+			else # -IncludeRegion not specified
+			{
+				return $arn
+			}
+			break
+		}
 	}
 }
 
-return $arn
+
